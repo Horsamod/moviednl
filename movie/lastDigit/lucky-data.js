88f1cc4dd40a62678729471150f4be95f9c51922
Horsamod <66909591+Horsamod@users.noreply.github.com>
2022-12-25 12:25:50 +0530

    update
    25/12/22

--- movie/lastDigit/lucky-data.js
+++ movie/lastDigit/lucky-data.js
@@ -1,10 +1,14 @@
-const targetNumber = [
+const dateTime = [
     {
         date: "30-12-22",
+        time: "12:00 PM",
+    },
+];
+const targetNumber = [
+    {
         mor: "48",
         day: "95",
         eve: "25",
         ext: "",
     },
-
 ];
